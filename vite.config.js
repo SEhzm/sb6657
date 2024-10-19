a49a1ad315a16743b1efdf9578be591e81c99816
hzming1 <13068475+hzming1@user.noreply.gitee.com>
2024-10-19 15:20:40 +0800

    test:测试https

--- vite.config.js
+++ vite.config.js
@@ -20,7 +20,7 @@ import ElementPlus from 'unplugin-element-plus/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({ 
-    base: '/sb6657.cn/',
+    base: '/',
     plugins: [
         vue(),
         AutoImport({
